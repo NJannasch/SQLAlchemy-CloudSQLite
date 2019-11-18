bc1b107f12eedef8133f31ddee97745b03217897
nils <nils.jannasch@tu-dortmund.de>
2019-11-17 22:20:32 -0500

    Setup.py changed

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='sqlalchemy_cloudsqlite',
-      version='0.1',
-      description='Cloud baked SQLite for Serverless Computing',
+      version='0.1.0dev0',
+      description='SQLite for Serverless Computing',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/njannasch/SQLAlchemy-CloudSQLite',
       author='Nils Jannasch',
       author_email='nils@njann.de',
@@ -13,5 +18,6 @@ setup(name='sqlalchemy_cloudsqlite',
           "sqlalchemy.dialects": [
               "cloudsqlite = sqlalchemy_cloudsqlite.dialect:CloudSQLiteDialect",
           ],
-      }
+      },
+      python_requires='>=3.6',
       )
